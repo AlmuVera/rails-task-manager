7117a0e1c469eed01cc3cdc54a8d54d1c89518a4
Almudena Vera <almudena.vigara@gmail.com>
2018-02-21 12:23:59 +0100

    added the routes, controller and views of the task list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+
+  def index         # GET /tasks
+    @tasks = Task.all
+  end
+
+  def show          # GET /tasks/:id
+    @task = Task.find(params[:id])
+  end
+
+  def new           # GET /tasks/new
+    @task = Task.new
+  end
+
+  def create        # POST /tasks
+    @task = Task.create(task_params)
+    @task.save
+    redirect_to root_path(@task)
+  end
+
+  def edit          # GET /tasks/:id/edit
+    @task = Task.find(params[:id])
+  end
+
+  def update        # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to root_path(@task)
+  end
+
+  def destroy       # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details,)
+
+  end
+end
